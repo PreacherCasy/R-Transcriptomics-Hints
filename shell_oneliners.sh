a756db7b5c3c07145d7eab89e6760612fbdb4bde
PreacherCasy <271296251017a@gmail.com>
2019-11-20 19:26:41 +0300

    Update shell_oneliners.sh

--- shell_oneliners.sh
+++ shell_oneliners.sh
@@ -2,3 +2,6 @@
 
 ### extract best BLAST matches based on e-value, coverage and identity 
 sort -k1,1 -k12,12gr -k11,11g -k3,3gr output | sort -u -k1,1 --merge > bestHits
+
+### extract metadata and variants having passed filtration from a VCF file
+awk -F '\t' '{if($0 ~ /\#/) print; else if($7 == "PASS") print}' input > output
